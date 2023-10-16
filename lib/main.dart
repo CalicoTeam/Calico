a1e6bdaf0a354886e1d1144dcce4010d5c68faa7
seoyoungk0909 <skimbj@connect.ust.hk>
2023-10-16 17:30:01 +0800

    login

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,34 @@
 import 'package:clovi_template/views/home_page.dart';
 import 'package:clovi_template/views/shorts_video_page.dart';
 import 'package:flutter/material.dart';
+import 'views/login_page.dart';
 import 'views/video_page.dart';
 import 'views/item_detail_page.dart';
 import 'views/item_info_page.dart';
+import 'utils/color_utils.dart';
 
 void main() {
-  runApp(const MyApp());
+  String initialRoute = "login";
+  runApp(MyApp(initialRoute: initialRoute));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key, required this.initialRoute}) : super(key: key);
+  final String initialRoute;
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primaryColor: CalicoColors.representBlack,
+        primaryColorLight: CalicoColors.representWhite,
+        highlightColor: CalicoColors.representOrange,
+      ),
       debugShowCheckedModeBanner: false,
-      initialRoute: '/',
+      initialRoute: initialRoute,
       routes: {
+        'login': (context) => LoginPage(),
         'video': (context) => VideoPage(),
         'item_info': (context) => ItemInfoPage(),
         'item_detail': (context) => ItemDetailPage(),
---
+++ lib/views/login_page.dart
@@ -0,0 +1,176 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      color: Colors.black,
+      child: Column(children: [
+        Padding(
+          padding: EdgeInsetsDirectional.only(top: 100, bottom: 100),
+          // height: 200,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            // crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: 250,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // Text("Welcome!"), Text("Please sign in to continue.")
+                    Text("Welcome!",
+                        style: TextStyle(
+                            color: Theme.of(context).primaryColorLight)),
+                    Text("Please sign in to continue.",
+                        style: TextStyle(
+                            color: Theme.of(context).primaryColorLight)),
+                  ],
+                ),
+              ),
+              // Container(
+              //   child:
+              Text("Logo",
+                  style: TextStyle(color: Theme.of(context).primaryColorLight)),
+
+              //   decoration: BoxDecoration(
+              //       image: DecorationImage(
+              //         image: AssetImage("assets/logo_rough.png"),
+              //         fit: BoxFit.cover,
+              //       ),
+              //       borderRadius: BorderRadius.circular(5)),
+              // ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30),
+              ),
+            ),
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Email',
+                  style: TextStyle(fontSize: 16.0),
+                ),
+                SizedBox(height: 5.0),
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border(bottom: BorderSide(color: Colors.black)),
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                Text(
+                  'Password',
+                  style: TextStyle(fontSize: 16.0),
+                ),
+                SizedBox(height: 5.0),
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border(bottom: BorderSide(color: Colors.black)),
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: TextButton(
+                    onPressed: () {
+                      // Forgot password button action
+                    },
+                    child: Text(
+                      'Forgot password?',
+                      style: TextStyle(
+                        color: Theme.of(context).highlightColor,
+                      ),
+                    ),
+                    style: ButtonStyle(
+                      overlayColor:
+                          MaterialStateProperty.all<Color>(Colors.transparent),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(0.0),
+                          side: BorderSide(color: Colors.transparent),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 30.0),
+                ElevatedButton(
+                  onPressed: () {
+                    // TODO: Login button action
+                    Navigator.pushNamed(context, '/');
+                  },
+                  child: Padding(
+                    padding: const EdgeInsetsDirectional.only(
+                        top: 16.0, bottom: 16.0),
+                    child: Text('Login'),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    fixedSize: Size.fromWidth(500),
+                    backgroundColor: Theme.of(context).highlightColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 30.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Don't have an account? "),
+                    TextButton(
+                      onPressed: () {
+                        // Sign up button action
+                      },
+                      child: Text(
+                        'Sign up',
+                        style:
+                            TextStyle(color: Theme.of(context).highlightColor),
+                      ),
+                      style: ButtonStyle(
+                        overlayColor: MaterialStateProperty.all<Color>(
+                            Colors.transparent),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(0.0),
+                            side: BorderSide(color: Colors.transparent),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ]),
+    ));
+  }
+}
